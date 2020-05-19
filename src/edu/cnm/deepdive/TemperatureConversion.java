00f3f29531f728f15b38781873e5e9d2746fa475
Mickie Morlang <mickiemorlang@gmail.com>
2020-05-19 10:03:56 -0600

    Strings of Arrays and their values.

--- src/edu/cnm/deepdive/TemperatureConversion.java
+++ src/edu/cnm/deepdive/TemperatureConversion.java
@@ -2,12 +2,16 @@ package edu.cnm.deepdive;
 
 public class TemperatureConversion {
 
+private static final double CELSIUS_TO_FAHRENHEIT_SCALE = 9.0 / 5.0;
+  private static final double FAHRENHEIT_TO_CELSIUS_SCALE = 1 / CELSIUS_TO_FAHRENHEIT_SCALE;
+  private static final double SCALE_OFFSET = 32;
+
   public static void main(String[] args) {
     if (args.length == 0) {
-      double c = 100;
-      double f = 32;
-      System.out.println("Celsius" + c + " = " + convertC2F(c) + " Fahrenheit");
-      System.out.println("Fahrenheit" + f + " = " + convertF2C(f) + " Celsius");
+      double tempCelsius = 100;
+      double tempFahrenheit = 32;
+      System.out.println("Celsius" + tempCelsius + " = " + convertC2F(tempCelsius) + " Fahrenheit");
+      System.out.println("Fahrenheit" + tempFahrenheit + " = " + convertF2C(tempFahrenheit) + " Celsius");
     } else {
 
       for (int i = 0; i < args.length; i++) {
@@ -21,10 +25,26 @@ public class TemperatureConversion {
 
 
   public static double convertC2F(double celsius) {
-    return celsius * 9 / 5 +32;
+    return celsius * CELSIUS_TO_FAHRENHEIT_SCALE +SCALE_OFFSET;
   }
 
 public static double convertF2C(double fahrenheit) {
-    return (fahrenheit - 32) * 5 / 9;
+    return (fahrenheit - SCALE_OFFSET) * FAHRENHEIT_TO_CELSIUS_SCALE;
   }
+
+
+
+public static double[] convertC2F(double[] celsiusTemperatures) {
+  double[] fahrenheitTemperatures = new double[celsiusTemperatures.length];
+  for (int i = 0; i > celsiusTemperatures.length; i++) {
+    double celsius = celsiusTemperatures[1];
+    double fahrenheit = convertC2F(celsius);
+    fahrenheitTemperatures[i] = fahrenheit;
+   }
+   return fahrenheitTemperatures;
+  }
+
+
 }
+
+
